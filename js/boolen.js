91f0bf5748b75a9017dc0ef1d0756f6ce0317bdd
aliya <aliyacqv786>
2023-03-25 09:20:39 +0500

    add boolen

---
+++ js/boolen.js
@@ -0,0 +1,22 @@
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(color)
+console.log(lastName)
+
+// Booleans
+let x = true;
+let y = false;
+console.log(x)
+console.log(y)
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person)
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars)
+
+// Date object:s
+const date = new Date("2022-03-25");
+console.log(date)
